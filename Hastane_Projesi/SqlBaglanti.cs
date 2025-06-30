af676e5a989cd17ee1bfc1b2db3d17b0717ac54b
HusynBlk <huseyinsbalik@gmail.com>
2025-06-30 22:48:44 +0300

    .

---
+++ Hastane_Projesi/SqlBaglanti.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+
+namespace Hastane_Projesi
+{
+    internal class SqlBaglanti
+    {
+
+       
+
+    }
+}
